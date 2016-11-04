67d5fc9de3855f872e02ee47417e5f4a0c7fa73b
Alvaro J. Hurtado <alvaroh@aspgems.com>
2016-11-04 12:27:10 +0100

    Field formatter added: calculated expression

---
+++ src/Plugin/Field/FieldFormatter/CalculatedExpression.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\mathematical_text_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+
+/**
+ * Plugin implementation of the 'calculated_expression' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "calculated_expression",
+ *   label = @Translation("Calculated expression"),
+ *   field_types = {
+ *     "text",
+ *     "text_long",
+ *     "string",
+ *     "string_long"
+ *   }
+ * )
+ */
+class CalculatedExpression extends FormatterBase {
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return array(
+      // Implement default settings.
+    ) + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return array(
+      // Implement settings form.
+    ) + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = ['#markup' => $this->viewValue($item)];
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    // The text value has no text format assigned to it, so the user input
+    // should equal the output, including newlines.
+    return nl2br(Html::escape($item->value));
+  }
+
+}
