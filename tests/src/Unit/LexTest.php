96b75c5e90bd38a9c4fe921883e9942e0f919778
Alvaro J. Hurtado <alvaroh@aspgems.com>
2016-11-04 15:07:04 +0100

    starting the tests

---
+++ tests/src/Unit/LexTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\Tests\mathematical_text_formatter\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+class LexTest extends UnitTestCase {
+  /**
+   * Test Lexer::Tokenize method.
+   */
+  public function testTokenize(){
+
+  }
+}
